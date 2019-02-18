af261e1cdd7753ef3ed8f3a664131e37e79758f4
Elizabeth Codd <elizabethcodd@Elizabeths-MacBook-Pro.local>
2019-02-18 17:04:34 +0000

    trying cors error

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(handle422);
 app.use(handle404);
 app.use(handle500);
 
-app.get("/products/:id", function(req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
-});
+// app.get("/products/:id", function(req, res, next) {
+//   res.json({ msg: "This is CORS-enabled for all origins!" });
+// });
 
 // app.listen(80, function() {
 //   console.log("CORS-enabled web server listening on port 80");
