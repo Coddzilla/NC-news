c45e1b1ac2fed2fc26b9622f7c107d167946c6da
Elizabeth Codd <elizabethcodd@Elizabeths-MacBook-Pro.local>
2019-03-01 11:28:48 +0000

    reseeded and changed string length in avatar_url

--- db/migrations/20190128123818_create_users.js
+++ db/migrations/20190128123818_create_users.js
@@ -1,12 +1,14 @@
-
 exports.up = function(connection, Promise) {
-  return connection.schema.createTable('users', (table) => {
-table.string('username').primary().notNullable();
-table.string('avatar_url').notNullable();
-table.string('name').notNullable();
-  })
+  return connection.schema.createTable("users", table => {
+    table
+      .string("username")
+      .primary()
+      .notNullable();
+    table.string("avatar_url", 500).notNullable();
+    table.string("name").notNullable();
+  });
 };
 
 exports.down = function(connection, Promise) {
-  return connection.schema.dropTable('users');
+  return connection.schema.dropTable("users");
 };
